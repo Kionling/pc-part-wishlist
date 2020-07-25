eef5dae635cff2d13c81e15c7a22ffe5540d91d4
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-25 09:12:24 -0700

    Generating placeholder values in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
+INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
+INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
+INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
+INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
