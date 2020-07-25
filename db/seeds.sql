591d7d754045ada2f873510b30b65faf24263162
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-25 10:16:01 -0700

    finished seeds.sq l

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
-INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
-INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
-INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
-INSERT INTO pcparts (name, pcparts) VALUES ("Geforce 2070 super ti");
+INSERT INTO pcparts (name, pcpart) VALUES ("Geforce 2070 super ti", 0);
+INSERT INTO pcparts (name, pcpart) VALUES ("TOMAHAWK MAX by Gigabyte", 1);
+INSERT INTO pcparts (name, pcpart) VALUES ("VENGENCE 16GB RAM", 1);
+INSERT INTO pcparts (name, pcpart) VALUES ("EVGA Power supply 650 W", 1);
+INSERT INTO pcparts (name, pcpart) VALUES ("AMD Ryzen 7 3700X", 0);
