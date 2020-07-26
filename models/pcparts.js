2281e9fafe7ac817c66bc2a240bb477ba4d45b69
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-26 10:35:49 -0700

    Generated all, create, update, and delete function callbacks

--- models/pcparts.js
+++ models/pcparts.js
@@ -1,1 +1,29 @@
 var orm = require("../config/orm.js");
+
+
+var pcpart = {
+    all: function(cb) {
+      orm.all("pcparts", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("pcparts", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("pcparts", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("pcparts", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = pcpart;
