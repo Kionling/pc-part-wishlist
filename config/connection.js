463921568bb947cec6e2ca122b3da25471abb7b1
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-27 00:32:09 -0700

    Finalizing

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@
 var mysql = require("mysql");
 
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "pcpartsDB"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "pcpartsDB"
+// });
 
 // for Heroku
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
